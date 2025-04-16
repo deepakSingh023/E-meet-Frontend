b3da0e2315822a3485d92750acc9e71b4934871d
deepakSingh023 <dsinghrana71@gmail.com>
2025-04-16 20:47:48 +0530

    changes to meeting comp

--- src/components/meeting.jsx
+++ src/components/meeting.jsx
@@ -10,7 +10,7 @@ import {
 } from "../webrtc"; // adjust the path if needed
 import socket from "../sockets"; // your socket connection instance
 
-export default function meeting () {
+export default function Meeting () {
   const { id: meetingId } = useParams();
   const navigate = useNavigate();
   const currentUser = useSelector((state) => state.auth.user);
