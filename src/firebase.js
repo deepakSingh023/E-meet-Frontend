47e66e1cb50472d4cdd528572cb8b10aff687b34
deepaksingh023 <dsinghrana71@gmail.com>
2025-07-19 01:22:33 +0530

    correcting firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; // ✅ MISSING IMPORT
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBuCYHjd8wdqyOPQ6QqVdqL5TGRe2DMc5I",
   authDomain: "e-meet-app-4adb1.firebaseapp.com",
@@ -20,6 +18,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
+// ✅ Initialize Firestore
 const db = getFirestore(app);
 
 export { db };
